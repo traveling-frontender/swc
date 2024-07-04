ce4d2e74f33cdc677ef263febab96377288f5c9d
Daniil <andreevdaniil781@gmail.com>
2024-07-04 15:15:43 +0300

    adding dist

---
+++ dist/assets/avatar--I9bdTrH.js
@@ -0,0 +1,1 @@
+const s="/swc/assets/avatar-0Y_LUkCe.png";export{s as _};
---
+++ dist/assets/avatar-legacy-CdbgzalN.js
@@ -0,0 +1,1 @@
+System.register([],(function(e,t){"use strict";return{execute:function(){e("_","/swc/assets/avatar-0Y_LUkCe.png")}}}));
