8456002f0dd0e36e67bcc1a091ec4f1b1da2bede
Daniil <andreevdaniil781@gmail.com>
2024-07-07 17:17:15 +0300

    fix

--- dist/assets/AppCard-DDNK8kID.js
+++
@@ -1,1 +0,0 @@
-import{d as s,u as o,_ as t,o as n,c as r,Z as a}from"./index-DBdb3FSp.js";const d=s({name:"AppCard",setup(){const{t:e}=o();return{t:e}}}),c={class:"card"};function p(e,_,u,i,f,l){return n(),r("div",c,[a(e.$slots,"body",{},void 0,!0)])}const $=t(d,[["render",p],["__scopeId","data-v-d8d923ed"]]);export{$ as A};
