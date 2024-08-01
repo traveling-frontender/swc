d3aaa35a602783064c601bfead3d79a387f95415
traveling-frontender <daniil03022006dan@gmail.com>
2024-08-01 20:53:05 +0500

    fix responsive

---
+++ assets/client-5-CuBpdmbs.js
@@ -0,0 +1,1 @@
+import{o as e,c as t}from"./index-D0Zy7z6O.js";const o={xmlns:"http://www.w3.org/2000/svg",width:"305.333",height:"138.667",version:"1.0",viewBox:"0 0 229 104"};function n(r,c){return e(),t("svg",o)}const i={render:n};export{i as default,n as render};
