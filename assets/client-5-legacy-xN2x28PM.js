ebc9a6051b66b64fa07e31ac87c6259230720504
traveling-frontender <daniil03022006dan@gmail.com>
2024-08-05 14:34:45 +0500

    fix header

---
+++ assets/client-5-legacy-xN2x28PM.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-C8dI6DKd.js"],(function(e,t){"use strict";var r,n;return{setters:[e=>{r=e.o,n=e.c}],execute:function(){e("render",s);const t={xmlns:"http://www.w3.org/2000/svg",width:"305.333",height:"138.667",version:"1.0",viewBox:"0 0 229 104"};function s(e,s){return r(),n("svg",t)}e("default",{render:s})}}}));
