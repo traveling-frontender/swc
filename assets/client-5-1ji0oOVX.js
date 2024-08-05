2f209021ed58811fa3b952b0f5215cc6b1e6cd71
traveling-frontender <daniil03022006dan@gmail.com>
2024-08-05 14:50:24 +0500

    fix fontsize

---
+++ assets/client-5-1ji0oOVX.js
@@ -0,0 +1,1 @@
+import{o as e,c as t}from"./index-C7TWPQeG.js";const o={xmlns:"http://www.w3.org/2000/svg",width:"305.333",height:"138.667",version:"1.0",viewBox:"0 0 229 104"};function n(r,c){return e(),t("svg",o)}const i={render:n};export{i as default,n as render};
